45a5c7e2f3380d1338a094c2d4f570c75478c14f
bishkou <chedyhammami@yahoo.com>
2020-11-12 22:36:05 +0100

    navigator fix

--- public/script.js
+++ public/script.js
@@ -10,8 +10,26 @@ var peer = new Peer(undefined, {
     port: '443'
 });
 
-
 let myVideoStream;
+
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream => {
+    myVideoStream = stream;
+    addVideoStream(myVideo, stream);
+
+    peer.on('call', call => {
+        call.answer(stream); // Answer the call with an A/V stream.
+        const video = document.createElement('video');
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream);
+        });
+    });
+    socket.on('user-connected', (userId) => {
+        connectToNewUser(userId, stream);
+    });
+});
 
 navigator.mediaDevices.getUserMedia({
     video: true,
